9d0bc0d91036a04235546a41028f0e3f846113c1
dev7 <luis.paredes@softit.pe>
2022-03-18 13:26:13 -0500

    Muestra tipo de afectacion

---
+++ inka_account_affection_type/models/account_move_line.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+
+class AccountMoveAffectionType(models.Model):
+    _inherit = "account.move.line"
+
+    affection_id = fields.Many2one(
+        comodel_name='inka.account.affection.type',
+        string='Tipo de Afectación',
+    )
+
+
+
+
