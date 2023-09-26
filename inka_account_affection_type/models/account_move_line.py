6bf31f2673060f851b5377fb0840f407b78770fa
Roberto <robertov.systems@gmail.com>
2023-09-25 19:46:25 -0500

    [MOD] - Sin onchange product_id

--- inka_account_affection_type/models/account_move_line.py
+++ inka_account_affection_type/models/account_move_line.py
@@ -9,13 +9,3 @@ class AccountMoveAffectionType(models.Model):
         'inka.account.affection.type',
         string=u"T. Afectación",
     )
-
-    @api.onchange('product_id')
-    def _onchange_product_id(self):
-        if self.product_id:
-            tax_group = self.product_id.taxes_id and self.product_id.taxes_id[0].tax_group_id
-            if tax_group:
-                affection = self.env['inka.account.affection.type'].search(
-                    [('code_at', '=', tax_group.affection_id.code_at)], limit=1)
-                if affection:
-                    self.affection_id = affection.id
